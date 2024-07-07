762f7241da2c2d89368f82fe500d32215da65cef
Kusunoki9999 <To29Sku51@icloud.com>
2024-07-07 09:58:54 +0900

    ユーザ入力のURLをjsonに追加

--- bff/json_manager.py
+++ bff/json_manager.py
@@ -1,44 +1,31 @@
-from flask import Flask, request, jsonify
 import json
+import os
 
-app = Flask(__name__)
-json_file_path = 'id_sheet.json'
 
-def main():
-    f = open('id_sheet.json','r')
-    json_data = json.load(f)
+JSON_FILE_PATH = os.path.join(os.path.dirname(__file__), 'id_sheet.json')
 
 # 読み込み
 def load_json():
-    with open(json_file_path, 'r') as file:
-        return json.load(file)
+    with open(JSON_FILE_PATH, 'r') as file:
+            return json.load(file)
 
 # 書き込み
 def save_json(data):
-    with open(json_file_path, 'w') as file:
+    with open(JSON_FILE_PATH, 'w') as file:
         json.dump(data, file, indent=4)
 
-@app.route('/add_song', methods=['POST'])
-def add_song():
-    data = request.json
-    playlist_id = str(data['playlist_id'])
-    song_url = data['song_url']
+def add_song(playlist_id, song_url):
 
     playlists = load_json()
 
-    # playlistのチェック
+    # プレイリストのチェック
     if playlist_id not in playlists['playlists']:
         playlists['playlists'][playlist_id] = {
             "songs": []
         }
 
+    # 曲の追加
     playlists['playlists'][playlist_id]['songs'].append(song_url)
-
     save_json(playlists)
 
-    return jsonify(playlists)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-    
+    return playlists
