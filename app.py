cba7c9959ca7ab6313628bbc3e40d7263babfb49
luckytale <arata9918.2001@icloud.com>
2024-07-07 10:19:46 +0900

    YouTube API

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import app, Flask, render_template, redirect, request, url_for
+from googleapiclient.discovery import build
 
 app = Flask(__name__)
 
@@ -6,17 +7,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route("/playlist")
-def playlist():
-    return render_template('playlist.html')
-
 @app.route("/search")
 def search():
     return render_template('search.html')
 
+@app.route("/playlist")
+def playlist():
+    video_title = get_title(video_id)
+    return render_template('playlist.html', video_title=video_title)
+
 @app.route("/play")
 def play():
     return render_template('play.html')
+
+#YouTubeAPI
+#video_idはURLから取得できるように修正
+api_key = ""
+video_id = "-NFhRWhkp7I"
+
+def get_title(video_id):
+    youtube = build('youtube', 'v3', developerKey=api_key)
+    request = youtube.videos().list(part="snippet", id=video_id)
+    response = request.execute()
+    video_title = response['items'][0]['snippet']['title']
+    return video_title
 
 if __name__ == "__main__":
     app.run()
